32673e53eb4a8fdf5b500408dad42d6907a328b2
Fabrice Bacchella <fbacchella@spamcop.net>
2014-10-30 15:41:36 +0100

    Many updates

--- README.md
+++ README.md
@@ -6,7 +6,17 @@ It's made to be used through sudo, as it can take a path of allowed directory fo
 
 To use it, one should add in /etc/sudoers.d/docker:
 
-    Cmnd_Alias DOCKER_COMMANDS = /user/bin/dockerrun -p /etc/dockercommand *
-    User_Alias DOCKER_USERS = %docker
+    Cmnd_Alias DOCKER_COMMANDS = /usr/bin/dockerrun *
+    User_Alias DOCKER_USERS = ...
+    Defaults!DOCKER_COMMANDS env_file = /etc/dockerrun/environment
 
     DOCKER_USERS ALL = (root) DOCKER_COMMANDS
+
+The content of /etc/dockerrun/environment should then be :
+    DOCKERRUN_YAMLPATH=/etc/dockerrun
+
+The yaml files can contains variable that will be resolved at container creation time. A variable is set
+with a `-v name value` argument. The environment variable are available as `environment.*name*`
+
+It also allow to attach to a container if the `user value` is equal to the user who launched the command, read
+from `SUDO_UID`
--- dockerrun.py
+++ dockerrun.py
@@ -18,13 +18,19 @@ import stat
 import string
 import collections
 from pwd import getpwnam
+import re
+
+# a template that allows . in variables name
+class DotTemplate(string.Template):
+    delimiter = '$'
+    idpattern = r'[_a-z][_a-z0-9\.]*'
+
 
 class DockerOption(optparse.Option):
     ACTIONS = optparse.Option.ACTIONS + ("store_first", "store_variable", )
     STORE_ACTIONS = optparse.Option.STORE_ACTIONS + ("store_first", "store_variable", )
     TYPED_ACTIONS = optparse.Option.TYPED_ACTIONS + ("store_first", "store_variable", )
     ALWAYS_TYPED_ACTIONS = optparse.Option.ALWAYS_TYPED_ACTIONS + ("store_first", "store_variable", )
-
 
     def __init__(self, *args, **kwargs):
         if kwargs['action'] == "store_variable":
@@ -48,41 +54,45 @@ class DockerOption(optparse.Option):
             optparse.Option.take_action(self, action, dest, opt, value, values, parser)
 
 
-def main():
-    parser = optparse.OptionParser(option_class=DockerOption)
-    parser.add_option("-p", "--p", dest="path", help="allowed path for yaml files", default="", action="store_first")
-    parser.add_option("-u", "--url", dest="url", help="base URL for docker connection", default=None, action="store")
-    parser.add_option("-s", "--socket", dest="socket", help="docker socket", default=None, action="store")
-    parser.add_option("-a", "--api", dest="api_version", help="docker api version", default='1.13', action="store_first")
-    parser.add_option("-t", "--timeout", dest="timeout", help="docker timeout", default=5, action="store", type="int")
-    parser.add_option("-v", "--variable", dest="variables", action="store_variable", type="string")
-
-    (options, args) = parser.parse_args()
-
+def run(docker, path, yamls, variables):
     allowed_paths = []
-    for element in options.path.split(os.pathsep):
-        allowed_paths.append( os.path.normcase(os.path.abspath(element)) )
+    if path is not None:
+        for element in path.split(os.pathsep):
+            allowed_paths.append( os.path.normcase(os.path.abspath(element)) )
 
-    docker = dockerlib.Client(base_url=options.url,
-                                  version=options.api_version,
-                                  timeout=options.timeout)
-
-    for docker_file_name in args:
+    for docker_file_name in yamls:
 
         # Check if the docker yaml file is in an allowed path
         if len(allowed_paths) > 0:
-            real_file_name = os.path.normcase(os.path.abspath(docker_file_name))
+            # no path given, look in allowed path and use it as the file name
+            # path is checked any way, to resolve symbolic links
+            if os.path.basename(docker_file_name) == docker_file_name:
+                for element in allowed_paths:
+                    if os.path.exists(element + os.sep + docker_file_name):
+                        # use that as the new file name
+                        docker_file_name = element + os.sep + docker_file_name
+                        break
+            # Resolve to a absolute real path
+            # symbolic links are resolve too
+            real_file_name = os.path.normcase(os.path.realpath(docker_file_name))
             valid = False
             for element in allowed_paths:
-                if os.path.commonprefix([element, real_file_name ]) == element:
+                if os.path.commonprefix([element, real_file_name]) == element:
                     valid = True
             if not valid:
                 print "invalid yaml container: %s" % docker_file_name
                 return 1
 
         #loads the file
-        docker_file = open(docker_file_name, 'r')
-        docker_kwargs = yaml.load(docker_file)
+        with open(docker_file_name, 'r') as docker_file:
+            content = docker_file.read()
+            content = re.sub(r'^#.*\n', '\n', content)
+            template = DotTemplate(content)
+            try:
+                content = template.substitute(variables)
+            except KeyError as e:
+                print e
+            docker_kwargs = yaml.load(content)
 
         effective_create_kwargs = {}
         effective_start_kwargs = {}
@@ -102,16 +112,13 @@ def main():
 
         # if a script is given, save it as a temporary file
         if 'script' in docker_kwargs:
-            if len(options.variables) > 0:
-                template = string.Template(docker_kwargs.pop('script'))
-                script = template.substitute(options.variables)
-            else:
-                script = docker_kwargs.pop('script')
+            script = docker_kwargs.pop('script')
             with tempfile.NamedTemporaryFile(delete=False) as script_file:
                 script_file.write(script)
                 script_file.flush()
-                os.chmod(script_file.name, (stat.S_IRWXU|stat.S_IRWXG|stat.S_IRWXO) & ~stat.S_IWUSR & ~stat.S_IWGRP & ~stat.S_IWOTH)
-                docker_kwargs['command'] = [ script_file.name ]
+                os.chmod(script_file.name,
+                         (stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO) & ~stat.S_IWUSR & ~stat.S_IWGRP & ~stat.S_IWOTH)
+                docker_kwargs['command'] = [script_file.name]
             if 'binds' not in docker_kwargs:
                 docker_kwargs['binds'] = collections.OrderedDict()
             docker_kwargs['binds'][script_file.name] = {'bind': script_file.name, 'ro': True}
@@ -130,22 +137,14 @@ def main():
         #start with 1, 0 is 'self'
         for arg_name in inspect.getargspec(dockerlib.Client.create_container).args[1:]:
             if arg_name in docker_kwargs:
-                value = docker_kwargs.pop(arg_name)
-                if isinstance(value, str) or isinstance(value, unicode):
-                    template = string.Template(value)
-                    value = template.substitute(options.variables)
-                effective_create_kwargs[arg_name] = value
+                effective_create_kwargs[arg_name] = docker_kwargs.pop(arg_name)
         #start with 1, 0 is 'self'
         for arg_name in inspect.getargspec(dockerlib.Client.start).args[1:]:
             if arg_name in docker_kwargs:
-                value = docker_kwargs.pop(arg_name)
-                if isinstance(value, str) or isinstance(value, unicode):
-                    template = string.Template(value)
-                    value = template.substitute(options.variables)
-                effective_start_kwargs[arg_name] = value
+                effective_start_kwargs[arg_name] = docker_kwargs.pop(arg_name)
 
         # is a numeric id given for the user, or is it needed to resolve it ?
-        if 'user' in effective_create_kwargs and not effective_create_kwargs['user'].isdigit():
+        if 'user' in effective_create_kwargs and not isinstance( effective_create_kwargs['user'], ( int, long ) ):
             user = effective_create_kwargs['user']
             try:
                 effective_create_kwargs['user'] = getpwnam(user).pw_uid
@@ -163,6 +162,71 @@ def main():
 
         if attach:
             os.execlp("docker", "docker", "attach", container['Id'])
+
+
+def purge(docker):
+    for container in docker.containers(all=True):
+        docker.remove_container(container, force=True)
+    more_images = True
+    while more_images:
+        images = docker.images(all=True)
+        print images
+        more_images = len(images) > 0
+        if more_images:
+            docker.remove_image(images[0]['Id'], force=True)
+
+def attach(docker, container):
+    info = docker.inspect_container(container)
+    docker_user = info['Config']['User']
+    real_user = os.environ['SUDO_UID']
+    if docker_user == real_user:
+        os.execlp("docker", "docker", "attach", container)
+    else:
+        print "not you're own container"
+
+def main():
+    parser = optparse.OptionParser(option_class=DockerOption)
+    parser.add_option("-p", "--p", dest="path", help="allowed path for yaml files", default=None, action="store_first")
+    parser.add_option("-u", "--url", dest="url", help="base URL for docker connection", default=None, action="store")
+    parser.add_option("-s", "--socket", dest="socket", help="docker socket", default=None, action="store")
+    parser.add_option("-a", "--api", dest="api_version", help="docker api version", default='1.13', action="store_first")
+    parser.add_option("-t", "--timeout", dest="timeout", help="docker timeout", default=20, action="store", type="int")
+    parser.add_option("-v", "--variable", dest="variables", action="store_variable", type="string")
+
+    (options, args) = parser.parse_args()
+
+    # Override -p with content of DOCKERRUN_YAMLPATH
+    # it's used in sudoers
+    if 'SUDO_COMMAND' in os.environ:
+        if 'DOCKERRUN_YAMLPATH' in os.environ:
+            options.path = os.environ['DOCKERRUN_YAMLPATH']
+        else:
+            print "run in sudo but not DOCKERRUN_YAMLPATH defined"
+            return 1
+    docker = dockerlib.Client(base_url=options.url,
+                              version=options.api_version,
+                              timeout=options.timeout)
+
+    # add some values in the variables
+    options.variables['system.uid'] = os.geteuid()
+    for (name, value) in os.environ.items():
+        options.variables['environment.%s' % name] = value
+
+    if args[0] == "run":
+        run(docker, options.path, args[1:], options.variables)
+        return 0
+    elif args[0] == "info":
+        print docker.info()
+        return 0
+    elif args[0] == "purge":
+        purge(docker)
+        return 0
+    elif args[0] == "attach":
+        attach(docker, args[1])
+        return 0
+    else:
+        print "no action given"
+        return 1
 
 # no global name space pollution
 if __name__ == '__main__':
